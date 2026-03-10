07aa311056160f3d8f264e3ea8f2ead0ae10382e
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2026-03-09 23:36:01 -0500

    Configure GitHub Pages deployment

--- starlight/astro.config.mjs
+++ starlight/astro.config.mjs
@@ -4,9 +4,12 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://Ross-Thanscheidt.github.io',
+    base: '/Notes',
 	integrations: [
 		starlight({
 			title: 'Ross Notes',
+            pagefind: false,
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Ross-Thanscheidt/Notes' }],
 			sidebar: [
 				{
